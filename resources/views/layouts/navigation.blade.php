facbe5d05f4006ece28267394fc8b25d8dbed2f6
asyiraafnufail <asyiraaf4@gmail.com>
2026-04-18 10:17:59 +0700

    add naviagation for smartphone

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -74,6 +74,9 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('about')" :active="request()->routeIs('about')">
+                {{ __('About') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
